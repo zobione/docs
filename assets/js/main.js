383fabfc3df574c1eaa9d9def1768ffd2be88576
zobioneKenobite <sanchez.emilien@gmail.com>
2017-05-01 20:59:22 +0000

    deploying version bab37d544aa3cc94def92521dfe647125d9f2926

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+    /* ======= ScrollTo ======= */
+    $('a.scrollto').on('click', function(e){
+        
+        //store hash
+        var target = this.hash;
+                
+        e.preventDefault();
+        
+		$('body').scrollTo(target, 800, {offset: -50, 'axis':'y'});
+        //Collapse mobile menu after clicking
+		if ($('.navbar-collapse').hasClass('in')){
+			$('.navbar-collapse').removeClass('in').addClass('collapse');
+		}
+		
+	});
+
+
+});
